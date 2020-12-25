3d6a5539a0f39cb857346847c9180214c0e981f2
markerz99 <54119958+markerz99@users.noreply.github.com>
2020-12-25 19:38:12 +0700

    v1

---
+++ TestVB/Form1.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(158, 113)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(349, 39)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Genarate Hash"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button2.Location = New System.Drawing.Point(470, 158)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(37, 31)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "..."
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.SystemColors.Info
+        Me.TextBox1.Location = New System.Drawing.Point(158, 158)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(306, 31)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.ForeColor = System.Drawing.SystemColors.Info
+        Me.TextBox3.Location = New System.Drawing.Point(158, 22)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(611, 31)
+        Me.TextBox3.TabIndex = 7
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(126, 24)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "sha1 ของไฟล์ : "
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(158, 59)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(611, 39)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "Copy"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(10, 56)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(580, 39)
+        Me.Button4.TabIndex = 12
+        Me.Button4.Text = "get json test"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.ForeColor = System.Drawing.SystemColors.Info
+        Me.TextBox2.Location = New System.Drawing.Point(10, 19)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(580, 31)
+        Me.TextBox2.TabIndex = 13
+        Me.TextBox2.Text = "https://sv1.rc-ace.com/update/patcher.json"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(64, 162)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(88, 24)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "เลือกไฟล์ : "
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Location = New System.Drawing.Point(158, 204)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(611, 108)
+        Me.GroupBox1.TabIndex = 15
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Json check"
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(791, 325)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "sha1 Checked"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ TestVB/Form1.vb
@@ -0,0 +1,103 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Net.WebClient
+Imports System.ComponentModel
+Imports System.Security.Principal
+Imports System.IO.Compression
+Imports Microsoft.VisualBasic.CompilerServices
+Imports System.Security.Cryptography
+Imports System.Text
+Imports System.Web.Script.Serialization
+
+Public Class Form1
+    Dim cookie As Integer = 1
+    WithEvents webclient1 As New WebClient
+    Dim appPath As String = Application.StartupPath()
+    'HASH CHECKED
+    Private Function ByteArrayToString(ByVal arrInput() As Byte) As String
+        Dim sb As New System.Text.StringBuilder(arrInput.Length * 2)
+        For i As Integer = 0 To arrInput.Length - 1
+            sb.Append(arrInput(i).ToString("X2"))
+        Next
+        Return sb.ToString().ToLower
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim openFileDialog1 As New OpenFileDialog()
+
+        openFileDialog1.InitialDirectory = "x_pathfileforsend"
+        openFileDialog1.Filter = "0m files (*.0m)|*.0m|jmd files (*.jmd)|*.jmd|All files (*.*)|*.*"
+        openFileDialog1.FilterIndex = 5
+        openFileDialog1.RestoreDirectory = True
+
+        If openFileDialog1.ShowDialog() = DialogResult.OK Then
+            TextBox1.Text = openFileDialog1.FileName
+        End If
+        openFileDialog1.Dispose()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'status server
+        Try
+            Dim guycity As New SHA1Managed ' SHA1Managed / SHA256Managed
+            Dim fileBytes() As Byte = IO.File.ReadAllBytes(TextBox1.Text)
+            Dim hash() As Byte = guycity.ComputeHash(fileBytes)
+
+
+            Dim calculatedHash As String = ByteArrayToString(hash)
+            'MsgBox(calculatedHash = referenceHash) 'outputs True
+            'MsgBox(calculatedHash, MsgBoxStyle.Information, "test")
+            TextBox3.Text = calculatedHash
+        Catch ex As Exception
+            MsgBox("ห้ามว่างเปล่า", MsgBoxStyle.Information, "แจ้งเตือน")
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            Clipboard.SetText(TextBox3.Text)
+            MsgBox("copy success", MsgBoxStyle.Information, "แจ้งเตือน")
+        Catch ex As Exception
+            MsgBox("ห้ามว่างเปล่า", MsgBoxStyle.Information, "แจ้งเตือน")
+        End Try
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim uriString As String = TextBox2.Text
+        Dim uri As New Uri(uriString)
+        'make http
+        Dim Request As HttpWebRequest = HttpWebRequest.Create(uri)
+        Request.Method = "GET"
+        'get http
+        Dim Response As HttpWebResponse = Request.GetResponse()
+        'read http
+        Dim Read = New StreamReader(Response.GetResponseStream())
+        Dim Raw As String = Read.ReadToEnd()
+
+        Dim dict As Object = New JavaScriptSerializer().Deserialize(Of List(Of Object))(Raw)
+
+        For Each item As Object In dict
+            Dim cookie2, cookie3, cookie4 As String
+            cookie2 = item("ChecksumHash").ToString
+            cookie3 = item("File").ToString
+            cookie4 = item("Download").ToString
+            MsgBox(cookie2 & vbCrLf & cookie3 & vbCrLf & cookie4, MsgBoxStyle.Information, "TEST")
+
+        Next
+
+
+
+
+    End Sub
+
+   
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'guycitycyberpunk += 1
+        'Label3.Text = guycitycyberpunk
+    End Sub
+End Class
